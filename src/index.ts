56df3955234e27a4354ea3b1cc73efb320b911d3
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-04 17:08:36 +0700

    feat: use zod openapi

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,82 @@
-import { Hono } from "hono";
-import { dataAirplanes } from "./data/airplanes";
+import { AirplaneSchema, dataAirplanes } from "./data/airplanes";
+import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
-app.get("/", (c) => {
-  return c.json({ message: "Airplanes API" });
-});
+app.openapi(
+  createRoute({
+    method: "get",
+    path: "/",
+    responses: {
+      200: {
+        description: "Welcome message",
+        content: {
+          "application/json": {
+            schema: z.object({ message: z.string(), openapi: z.string() }),
+          },
+        },
+      },
+    },
+  }),
+  (c) => {
+    return c.json({
+      message: "Airplanes API",
+      openapi: "/openapi.json",
+    });
+  }
+);
 
-app.get("/airplanes", (c) => {
-  return c.json(dataAirplanes);
-});
+app.openapi(
+  createRoute({
+    method: "get",
+    path: "/airplanes",
+    responses: {
+      200: {
+        description: "Get all airplanes",
+        content: { "application/json": { schema: z.array(AirplaneSchema) } },
+      },
+    },
+  }),
+  (c) => {
+    return c.json(dataAirplanes);
+  }
+);
 
-app.get("/airplanes/:id", (c) => {
-  const id = Number(c.req.param("id"));
-  const airplane = dataAirplanes.find((airplane) => airplane.id === id);
-  return c.json(airplane);
+app.openapi(
+  createRoute({
+    method: "get",
+    path: "/airplanes/:id",
+    params: {
+      id: {
+        description: "Airplane ID",
+        required: true,
+        schema: z.number(),
+      },
+    },
+    responses: {
+      404: { description: "Airplane not found" },
+      200: {
+        description: "Get one airplane by ID",
+        content: { "application/json": { schema: AirplaneSchema } },
+      },
+    },
+  }),
+  (c) => {
+    const id = Number(c.req.param("id"));
+    const airplane = dataAirplanes.find((airplane) => airplane.id === id);
+
+    if (!airplane) return c.notFound();
+
+    return c.json(airplane);
+  }
+);
+
+app.doc("/openapi.json", {
+  openapi: "3.1.1",
+  info: {
+    title: "Airplanes API",
+    version: "1.0.0",
+  },
 });
 
 export default app;
